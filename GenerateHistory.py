8d535a0349a4a84167f97ad2bd5d9f84cf37836a
WalonLi <a63241924@gmail.com>
2017-10-29 21:07:51 +0800

    fix some bug

--- GenerateHistory.py
+++ GenerateHistory.py
@@ -12,31 +12,45 @@ import sys
 
 # type,code,name,ISIN,start,market,group,CFI
 # 股票,1101,台泥,TW0001101004,1962/02/09,上市,水泥工業,ESVUFR
-
+#
+# DATATUPLE = namedtuple('Data', ['date', 'capacity', 'turnover', 'open',
+#                                 'high', 'low', 'close', 'change', 'transaction'])
 class Global:
     wait_time = 6
+    skip = True # some stock information can't get.
 
+def handle_data(stock, y, m, raw_path, sheet, row):
+    Global.skip = True
 
-def handle_data(stock, y, m, sheet, row):
+    for loop in range(3):
+        try:
+            time.sleep(Global.wait_time) # small delay for website block
+            Global.wait_time -= 1
+            if Global.wait_time < 2:
+                Global.wait_time = 6
 
-    time.sleep(Global.wait_time) # small delay for website block
-    Global.wait_time -= 1
-    if Global.wait_time < 2:
-        Global.wait_time = 6
+            date_list = stock.fetch(y, m)
+            # if not stock.low:
+            #     raise ValueError
 
-    date_list = stock.fetch(y, m)
-    # if not stock.low:
-    #     raise ValueError
-
-    sheet.write_row('A' + str(row),
-                    [str(y) + " %02d"%m,
-                     min(stock.low),
-                     max(stock.high),
-                     date_list[0].open,
-                     date_list[-1].close,
-                     sum(stock.capacity),
-                     round(date_list[-1].close - date_list[0].open, 2),
-                     sum(stock.turnover)])
+            sheet.write_row('A' + str(row),
+                            [str(y) + " %02d"%m,
+                             min(stock.low),
+                             max(stock.high),
+                             date_list[0].open,
+                             date_list[-1].close,
+                             sum(stock.capacity),
+                             round(date_list[-1].close - date_list[0].open, 2),
+                             sum(stock.turnover)])
+            # flush raw data
+            file = open(raw_path, 'w')
+            for i in range(len(date_list)):
+                file.write(str(date_list[i])+'\n')
+            file.close()
+            Global.skip = False
+            break
+        except ValueError as e:
+            print('unknown value error.', e)
 
 
 history_head = ['日期', '最低', '最高', '開盤', '收盤', '成交量', '價差', '金額']
@@ -46,10 +60,10 @@ if __name__ == '__main__':
     # time.sleep(1)
     # stock = twstock.realtime.get('2412')
 
-    for number in range(1000, 2000, 1):
+    for number in range(1258, 2000, 1):
         if str(number) in twstock.codes:
 
-            time.sleep(2) # small delay for website block
+            # time.sleep(2) # small delay for website block
 
             stock_info = twstock.codes[str(number)]
             stock_name = stock_info[1] + re.sub('\*', '', stock_info[2]) # strip some special character
@@ -64,11 +78,11 @@ if __name__ == '__main__':
             if not os.path.isdir(path):
                 os.mkdir(path)
 
-            path += '//' + stock_name + '_History.xlsx'
-            if not os.path.isfile(path):
+            excel_path = path + '//' + stock_name + '_History.xlsx'
+            if not os.path.isfile(excel_path):
                 # if not exist, get all data
 
-                file = xlsxwriter.Workbook(path)
+                file = xlsxwriter.Workbook(excel_path)
                 sheet = file.add_worksheet()
                 sheet.write_row('A1', history_head)
 
@@ -82,17 +96,18 @@ if __name__ == '__main__':
 
                 stock = Stock(str(number))
 
-
                 print(stock_name, start_date)
-                if result.days > 16:
+                if result.days > (15*365):
                     # only handle recently 15 years
                     for i in range(15*12):
                         # handle data
-                        # year = 2017
-                        # month = 4
                         print('    %d %d parsing...' % (year, month))
-                        handle_data(stock, year, month, sheet, i+2)
-                        # sys.exit()
+                        handle_data(stock, year, month, path+'//raw%d%02d'%(year,month), sheet, i+2)
+                        if Global.skip:
+                            file = open(path+'//raw%d%02d.fail'%(year,month), 'w')
+                            file.close()
+                            break
+
                         month -= 1
                         if month == 0:
                             year -= 1
@@ -103,7 +118,11 @@ if __name__ == '__main__':
                     while True:
                         # handle data
                         print('    %d %d parsing...' % (year, month))
-                        handle_data(stock, year, month, sheet, i+2)
+                        handle_data(stock, year, month, path+'//raw%d%02d'%(year,month), sheet, i+2)
+                        if Global.skip:
+                            file = open(path+'//raw%d%02d.fail'%(year,month), 'w')
+                            file.close()
+                            break
 
                         if start_date.year == year and start_date.month == month:
                             print(start_date.year, start_date.month, ' end')
@@ -113,9 +132,9 @@ if __name__ == '__main__':
                         if month == 0:
                             year -= 1
                             month = 12
-                            print('    %d parsing...' % year)
-
                 file.close()
+
+
             else:
                 # if exist, just scan latest data.
                 pass
--- setup.bat
+++ setup.bat
@@ -2,4 +2,4 @@
 pip install twstock
 pip install xlutils
 pip install xlsxwriter
-copy patch\stock.py C:\Python36-32\Lib\site-packages\twstock\stock.py
+copy Patch\stock.py C:\Python36-32\Lib\site-packages\twstock\stock.py
